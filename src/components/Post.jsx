a53d738559d7e8788787d8c8e046a49a6cb99cec
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-07-04 12:13:55 -0300

    Componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,9 +15,34 @@ export function Post() {
           </div>
         </div>
 
-        <time>Publicado há 1hora</time>
-        
+        <time title="02 de Julho às 18:37" dataTime="">Publicado há 1hora</time>
+                
       </header>
+
+      <div className={styles.content}>
+         <p>Fala galera</p>
+         <p>Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat</p>
+         <p><a href="#"> 👉 jane.design/doctorcare</a></p>
+         <p>
+           <a href="">
+             <a>#novoprojeto</a>{' '}
+             <a>#nlw</a>{' '}
+             <a>#rocketseat</a>{' '}
+             </a>
+          </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
